a450a0586469735735e07cf7904a7f722c1adeb5
Raihanuzzaman Rasel <md.raihanbdrasel@gmail.com>
2025-01-15 03:18:57 +0600

    That's one way to do it

--- index.js
+++ index.js
@@ -16,11 +16,19 @@ typewriters.forEach(async (writer) => {
     const dataset = writer.getAttribute('data-typewriter').split(",").map(x => x.trim());
     for (let i = 0; i < dataset.length; i++) {
         const currentWord = dataset[i];
-        for (let j = 1; j <= currentWord.length; j++) {
+        let j = 1;
+        let isReverse = false;
+        while (j >= 1) {
             await new Promise(resolve => setTimeout(resolve, 100));
             writer.innerHTML = currentWord.slice(0, j);
+            if (j === currentWord.length) {
+                isReverse = true;
+                await new Promise(resolve => setTimeout(resolve, 1500));
+            }
+            if (isReverse) j--;
+            else j++;
         }
-        await new Promise(resolve => setTimeout(resolve, 1500));
+        await new Promise(resolve => setTimeout(resolve, 100));
         if (i === dataset.length - 1) i = -1;
     }
 });
