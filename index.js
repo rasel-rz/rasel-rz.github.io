6001d28eff8e1e9de76b889869b59b5b360ad201
Raihanuzzaman Rasel <md.raihanbdrasel@gmail.com>
2025-01-15 02:59:28 +0600

    Type Writer

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const emojiList = [
-    "⚫", "🔲", "⬛", "🏴", "♠️", "🖤", "🕶️",
-    "🟧", "🟠", "🍊", "🔶",  "🍂", "🧡", "🌅", "🦊", "📙",
-    "🔴", "🟥", "❤️", "🔥", "🍎", "🌹", "📕", "🧨", "🎈", "🛑",
-    "🟢", "🟩", "🌿", "🌱", "🍀", "🍃", "🪴", "📗", "🥦"
+    "⚫", "🔲", "⬛", "🏴", "♠️", "🖤", "🕶️", "🟧", "🟠", "🍊", "🔶", "🍂", "🧡", "🌅", "🦊", "📙", "🔴", "🟥", "❤️", "🔥", "🍎", "🌹", "📕", "🧨", "🎈", "🛑", "🟢", "🟩", "🌿", "🌱", "🍀", "🍃", "🪴", "📗", "🥦"
 ];
 
 const lists = document.querySelectorAll("ul");
@@ -11,7 +8,20 @@ lists.forEach(list => {
     list.querySelectorAll('li').forEach(item => {
         item.setAttribute("data-bullet", `${emojiList[randomIndex]}`);
     });
-    // list.style.setProperty("list-style-type", `"\\0${emojiList[randomIndex]}"`)
     emojiList.splice(randomIndex, 1);
+});
+
+const typewriters = document.querySelectorAll('[data-typewriter]');
+typewriters.forEach(async (writer) => {
+    const dataset = writer.getAttribute('data-typewriter').split(",").map(x => x.trim());
+    for (let i = 0; i < dataset.length; i++) {
+        const currentWord = dataset[i];
+        for (let j = 1; j <= currentWord.length; j++) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+            writer.innerHTML = currentWord.slice(0, j);
+        }
+        await new Promise(resolve => setTimeout(resolve, 1500));
+        if (i === dataset.length - 1) i = -1;
+    }
 });
 console.log("The script runs!");
